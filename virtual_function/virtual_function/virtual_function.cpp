c081e8dc2ad5f5fcd3f62187f4500e3e4e37a50b
selimsagir <selimsagir61@gmail.com>
2023-05-17 23:56:31 +0200

    virtual and template

---
+++ virtual_function/virtual_function/virtual_function.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+template<typename T, typename U>
+class Shape {
+private:
+    T x;
+    T y;
+
+public:
+    Shape()
+        : x(0), y(0)
+    {
+    }
+
+    ~Shape() = default;
+
+    T getX() {
+        return x;
+    }
+
+    T getY() {
+        return y;
+    }
+
+    void setX(T val) {
+        x = val;
+    }
+
+    void setY(T val) {
+        y = val;
+    }
+
+    virtual T ShapeArea(T h, U w) {
+        return h * w;
+    }
+};
+
+template<typename T>
+class Square : public Shape<T, T> {
+public:
+    T ShapeArea(T h, T w) override {
+        return h * w;
+    }
+};
+
+int main() {
+    Square<int> square;
+    square.setX(5);
+    square.setY(5);
+    int area = square.ShapeArea(5,5);
+    std::cout << "Area: " << area << std::endl;
+
+    return 0;
+}
